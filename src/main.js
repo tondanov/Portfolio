2bbe6571c8dbf3e4670005672c8c537357ffeaf4
tondanov <toneknovotny@gmail.com>
2024-03-20 17:32:48 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import "../styles/modern-normalize.css";
+import '../styles/style.css';
+import "../styles/components/header.css";
+import "../styles/components/hero.css";
+import "../styles/components/about.css";
+import "../styles/components/work.css";
+import "../styles/components/contacts.css";
+import "../styles/components/mobile-nav.css";
+import "../styles/utils.css";
+
+import mobileNav from "./utils/mobile-nav";
+
+mobileNav();
